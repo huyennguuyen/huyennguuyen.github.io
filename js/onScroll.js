9059094fac4b05f4baedf52fa72e678c0c57df19
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-06-09 12:37:44 -0700

    removed white background of gifs

--- js/onScroll.js
+++ js/onScroll.js
@@ -1,10 +1,12 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    let reveals = document.querySelectorAll(".reveal");
   
     for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+
+      console.log(reveals)
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
